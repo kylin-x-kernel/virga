8a14110a709c75dcaf0b9b77920ca9af2ec14a82
727Hsj <100742672+727Hsj@users.noreply.github.com>
2026-01-22 12:22:03 +0800

    Hsj/dev x (#2)
    * fix for xtransport
    
    * fix example when use the xtransport feature
    
    * remove some comments
    
    * remove some comments
    
    * modify the example
    
    * fix for xtransport
    
    * fix example when use the xtransport feature
    
    * remove some comments
    
    * remove some comments
    
    * modify the example

--- example/client_test/src/main.rs
+++ example/client_test/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{Read, Write};
+
 use virga::client::{VirgeClient, ClientConfig};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -6,11 +8,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = ClientConfig::new(2, 1234, 1024, false);
     let mut client = VirgeClient::new(config);
     client.connect()?;
+
+    // 处理发送数据, 先发送数据长度，然后发送数据
+    let data = vec![1; 512];
+    client.write(&data.len().to_be_bytes())?;
+    client.write(&data)?;
+
+
+    // 处理接收数据, 先接收数据长度，然后创建一个足够长的databuf，最后接收数据
+    let mut buf = [0u8; 8];
+    client.read(&mut buf)?;
+    let data_len = usize::from_be_bytes(buf);
     
-    client.send(vec![1; 512])?;
-    let data = client.recv()?;
-    println!("{}", data.len());
+    let mut data = vec![0; data_len];
+    let actual_data_len_ = client.read(&mut data)?;
+    assert_eq!(data_len, actual_data_len_);
+    println!("len date = {}", actual_data_len_);
     
+    // 断开连接
     client.disconnect()?;
+
     Ok(())
 }
--- example/server_test/src/main.rs
+++ example/server_test/src/main.rs
@@ -1,4 +1,4 @@
-use std::thread;
+use std::io::{Read, Write};
 use virga::server::{ServerManager, ServerConfig};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,32 +11,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     if let Ok(mut server) = manager.accept() {
         println!("there is a new virgeserver");
-        let handle = thread::spawn(move ||  {
-            // 处理接收数据
-            if server.is_connected(){
-                println!("after get virga server, the server is connected");
-            }
-            let data_result = server.recv();
-            let data = match data_result {
-                Ok(data) => data,
-                Err(e) => {
-                    eprintln!("接收数据失败: {}", e);
-                    return;  // 直接返回，不继续执行
-                }
-            };
-            println!("len date = {}", data.len());
-            
-            // 处理发送数据
-            if let Err(e) = server.send(data) {
-                eprintln!("发送数据失败: {}", e);
-            }
-            
-            // 处理断开连接
-            if let Err(e) = server.disconnect() {
-                eprintln!("断开连接失败: {}", e);
-            }
-        });
-        handle.join().unwrap();
+        
+        // 处理接收数据, 先接收数据长度，然后创建一个足够长的databuf，最后接收数据
+        let mut buf: [u8; 8] = [0u8; 8];
+        server.read(&mut buf)?;
+        let data_len = usize::from_be_bytes(buf);
+        println!("data_len: {data_len}");
+        
+        let mut data = vec![0; data_len];
+        let actual_data_len_ = server.read(&mut data)?;
+        assert_eq!(data_len, actual_data_len_);
+        
+        // 处理发送数据, 先发送数据长度，然后发送数据
+        server.write_all(&data.len().to_be_bytes())?;
+        server.write_all(&data)?;
+        
+        // 断开连接
+        server.disconnect()?;
     }
 
     Ok(())
